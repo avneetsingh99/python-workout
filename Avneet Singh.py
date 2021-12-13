5301cf8a0cd724d7e08e138109d1e4127561eb5b
avneetsingh99 <96041683+avneetsingh99@users.noreply.github.com>
2021-12-13 12:26:38 +0530

    Add files via upload

---
+++ Avneet Singh.py
@@ -0,0 +1,61 @@
+ask_user= str(input("Do you want to play the quiz:"))
+if (ask_user== "yes"):
+    print("que 1.: how many subjects in BCA options:")
+    print("1.four subjects")
+    print("2.three subjects")
+    print("3. one subject")
+    print("4. none of these")
+    write_your_answer=int(input("write your answer"))
+    if (write_your_answer == 1):
+        print("your answer is correct")
+        print("uh got 10 points")
+    else:
+     print("your answer is wrong")
+    if (ask_user== "yes"):
+     print("que 2.:which is the correct option for types of operating system:")
+     print("1.windows")
+     print("2.linux")
+     print("3.batch operating system")
+     print("4. none of these")
+    write_your_answer=int(input("write your answer"))
+    if (write_your_answer == 3):
+     print("your answer is correct")
+     print("uh got 10 points")
+    else:
+     print("your answer is wrong")
+    if (ask_user== "yes"):
+     print("que 3.:where is operating system in c.p.u:")
+     print("1.processor")
+     print("2.power supply")
+     print("3.input/output device")
+     print("4.main memory")
+     write_your_answer=int(input("write your answer"))
+    if (write_your_answer == 4):
+     print("your answer is correct")
+     print("uh got 10 points")
+    else:
+     print("your answer is wrong")
+    if (ask_user== "yes"):
+     print("que 3.:where is the main branch of SAITM:")
+     print("1.Delhi")
+     print("2.Gaziabad")
+     print("3.gurugram")
+     print("4.karol bagh")
+    write_your_answer=int(input("write your answer"))
+    if (write_your_answer == 3):
+     print("your answer is correct")
+     print("uh got 10 points")
+    else:
+     print("your answer is wrong")
+else:
+    print("bye!bye!")
+    
+    
+
+    
+
+
+    
+    
+          
+          
